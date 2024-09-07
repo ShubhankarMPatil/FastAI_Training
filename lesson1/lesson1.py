31b86dffdf5ba74486afcca9b1512f7b68cd83a6
Shubhankar Patil <shubhankarmpatil@gmail.com>
2024-09-07 17:44:23 +0530

    bird and forest photo retrieval

--- lesson1/lesson1.py
+++ lesson1/lesson1.py
@@ -2,17 +2,27 @@ from duckduckgo_search import DDGS
 from fastcore.all import *
 from fastdownload import download_url
 from fastai.vision.all import *
+from time import sleep
 
 def search_images(term, max_images = 30):
     print(f"Searching for '{term}'")
     return DDGS().images(keywords=term, max_results = max_images)
 
-urls = search_images('bird photos', max_images = 100)
-urls = list(urls)
+bird_urls = search_images('bird photos', max_images = 0)
+forest_urls = search_images('forest photos', max_images = 7)
+b_urls = list(bird_urls)
+f_urls = list(forest_urls)
 
-for i in urls:
-    # download_url(i.get('image'), 'bird.jpg', show_progress=False)
-    # im = Image.open('bird.jpg')
-    # im.to_thumb(256, 256)
+count = 39
 
-    print(i.get('image'))
+for i in b_urls:
+    download_url(i.get('image'), f'birds/bird_{count}.jpg', show_progress=False)
+    count = count + 1
+    sleep(1)
+
+count = 44
+
+for i in f_urls:
+    download_url(i.get('image'), f'forest/forest_{count}.jpg', show_progress=False)
+    count = count + 1
+    sleep(1)
