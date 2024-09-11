d3d0a53c3ae56086533887f23dfa1cb58d3924b2
Shubhankar Patil <shubhankarmpatil@gmail.com>
2024-09-11 20:14:36 +0530

    seperate image collection file

--- lesson1/fetchImages.py
+++ lesson1/fetchImages.py
@@ -8,21 +8,21 @@ def search_images(term, max_images = 30):
     print(f"Searching for '{term}'")
     return DDGS().images(keywords=term, max_results = max_images)
 
-bird_urls = search_images('bird photos', max_images = 0)
-forest_urls = search_images('forest photos', max_images = 7)
+bird_urls = search_images('bird photos', max_images = 50)
+forest_urls = search_images('forest photos', max_images = 50)
 b_urls = list(bird_urls)
 f_urls = list(forest_urls)
 
-count = 39
+count = 0
 
 for i in b_urls:
     download_url(i.get('image'), f'birds/bird_{count}.jpg', show_progress=False)
     count = count + 1
-    sleep(1)
+    sleep(5)
 
-count = 44
+count = 0
 
 for i in f_urls:
     download_url(i.get('image'), f'forest/forest_{count}.jpg', show_progress=False)
     count = count + 1
-    sleep(1)
+    sleep(5)
